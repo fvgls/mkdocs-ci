60f7167b436774ceda63eb6ced7578c12855606e
fvgls <38109172+fvgls@users.noreply.github.com>
2020-01-15 16:06:27 +0100

    adding push in git procedure

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "fannyvignolles@gmail.com"
+  git config --global user.name "fvgls"
+}
+
+commit_website_files() {
+  git checkout -b docs
+  git add . 
+  git commit --message "Generating docs / Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/fvgls/mkdocs.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages docs 
+}
+
+setup_git
+commit_website_files
+upload_files
