1224def4de03d2f866ba72a90fc3f3d5fe64a24b
fvgls <fannyvignolles@gmail.com>
2020-01-15 16:20:18 +0100

    correcting GITHUB_TOKEN

--- .travis/push.sh
+++ .travis/push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/fvgls/mkdocs.git > /dev/null 2>&1
+  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/fvgls/mkdocs.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages docs 
 }
 
